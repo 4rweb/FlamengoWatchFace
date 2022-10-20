4bdf838a72c7809df9d4c7392be1a8476c9776b5
4rweb <info@4rweb.net>
2022-10-20 07:03:08 -0300

    update readme

--- README.m
+++ README.m
@@ -1,1 +1,30 @@
 # FlamengoWatchFace
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/4rweb/FlamengoWatchFace/main/assets/preview.png" width="200" />
+</p>
+
+<h1 align="center">
+  Flamengo WatchFace 
+</h1>
+
+This is a watchface created for the Zepp OS system and can be downloaded on your GTS or GTR smartwatch.
+
+
+## Building
+
+All you need to get started is Node.js 14.x+ on macOS, Windows(WSL) or Linux.
+
+
+```bash
+install npm i @zeppos/zeus-cli -g
+
+# Start development and debugging
+zeus dev
+
+# Start the real machine preview
+zeus preview
+
+# Build project package
+zeus build
+```
